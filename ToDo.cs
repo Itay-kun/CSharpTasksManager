84cb2df004411d1f29654233ddbae051e504b940
Itay <Itay-kun@users.noreply.github.com>
2024-04-01 15:46:01 +0300

    Add files via upload

---
+++ ToDo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class ToDoItem
+    {
+        public string Key { get; private set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ParentTaskKey { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        
+        public ToDoItem(string title, string description, string parentTaskKey, DateTime startDate, DateTime endDate)
+        {
+            Key = DateTime.Now.ToString("yyMMddHHmmss");
+            Title = title;
+            Description = description;
+            ParentTaskKey = parentTaskKey;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public override string ToString()
+        {
+            return $"Key: {Key}, Title: {Title}, Description: {Description}, " +
+                   $"ParentTaskKey: {ParentTaskKey}, StartDate: {StartDate}, EndDate: {EndDate}";
+        }
+    }
+}
